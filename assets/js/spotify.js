d2a4cb34500954fec1369899604b2185b044ee1f
sungl4ss <g1asstech@outlook.com>
2024-02-05 18:47:09 -0800

    update

---
+++ assets/js/spotify.js
@@ -0,0 +1,48 @@
+(function () {
+  var timeouts = [];
+
+  if ($.cookie("videoTime")) {
+    app.videoElement.currentTime = $.cookie("videoTime");
+  }
+
+  document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+  });
+
+  $(".next").click(function () {
+    next();
+  });
+
+  timeouts.forEach(function (timeout) {
+    clearTimeout(timeout);
+  });
+
+  timeouts.forEach(function (timeout) {
+    clearTimeout(timeout);
+  });
+
+  $(".startthing").remove();
+
+  setTimeout(function () {
+    var typed = new Typed("#Bruh", {
+      strings: app.Texts,
+      typeSpeed: 40,
+      backDelay: 500,
+      onComplete: function () {
+        return $("span").siblings(".typed-cursor").css("opacity", "0");
+      },
+    });
+  }, 1350);
+
+  setTimeout(function () {
+    app.videoElement.play();
+
+    $(".navbar").css("visibility", "visible").hide().fadeIn(500);
+    $(".background").css("visibility", "visible").hide().fadeIn(500);
+    $(".main").css("visibility", "visible").hide().fadeIn(500);
+
+    $(".background").fadeIn(200, function () {
+      $("#background").animate({ volume: app.musicVolume }, app.musicFadeIn);
+    });
+  }, 200);
+})();
